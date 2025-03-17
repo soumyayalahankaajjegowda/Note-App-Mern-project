9013492f5dfb26f490be08cb396b34a9bc85ea8f
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-17 17:10:47 -0600

    completed AddNote

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+function App() {
+  return ( 
+  < >
+    <Router>
+      <Head />
+      <Routes>
+         <Route path={""}/>
+      </Routes>
+    </Router>
+  </>
+  );
+}
+
+export default App;
