f74bb5a32f51d0f85e90fda3fc4b1bcb00559366
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-17 10:48:16 -0600

    controllers are set

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,3 +1,19 @@
 //import Model
 const Message = require ("../models/message");
 
+//controllers
+
+
+//To submit message through contact form
+const submit_feedback = (req, res) => {
+  let message = new Message(req, body);
+  message
+  .save()
+  .then((result) => {
+    res.json({msg: error.message });
+  })
+  .catch((error) => res.json({msg: error.message }));
+};
+
+//Exports
+module.exports = { submit_feedback };
---
+++ backend/controllers/noteControllers.js
@@ -0,0 +1,74 @@
+//Importing Models
+const Note = require("../models/note");
+
+
+//to retrieve all notes from the database
+const get_all_notes = (req, res) => {
+  Note.find()
+  .sort({ createdAt: -1 })
+  .then((result) => {
+    if(result.length > 0) {
+      res.json({
+        msg:"All notes have been fetched successfully!",
+        content: result,
+      });
+    }else {
+      res.json({ msg: "No notes to show!" });
+    }
+    })
+    .catch((error) => res.json({ msg: error.message}));
+};
+
+//To add a new note to the database
+const add_note = (req, res) => {
+  let note = new Note(req.body);
+  note
+  .save()
+  .then((result) => {
+    res.json({
+      msg:"Your note was saved successfully!",
+     content: result,
+    });
+  })
+  .catch((error) => res.json({ msg: error.message }));
+};
+
+//To retrive a singlw note by its ID
+const get_one_note = (req, res) => {
+  const id = req.params.id;
+  Note.findBy(id)
+  .then((result) => {
+    if (result !=null) {
+      res.json({
+        msg:"The note was fetched successfully!",
+        content: result,
+      });
+    } else {
+      res.json({ msg: "This note dosn't exits" });
+    }
+  })
+  .catch((error) => res.json({ msg:error.message }));
+};
+
+    //To edit an existing note
+    const update_note = (req, res)=> {
+      const id = req.params.id;
+      Note.findByIdAndDelete(id)
+      .then((result => {
+        if (result != null) {
+          res.json({msg: "This note was successfully deleted!" });
+        } else {
+          res,json({ msg: "This note dosen't exist!"});
+        }
+      })
+      .catch((error) => res.json({ msg: error.message }));
+    };
+
+    //Exports
+    module.exports = {
+      get_all_notes,
+      add_note,
+      get_one_note,
+      update_note,
+      delete_note,
+    };
