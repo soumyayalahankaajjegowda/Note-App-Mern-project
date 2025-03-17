fc22b809e91d14c4f124ae208dfb110c14a0bdf9
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-17 15:25:20 -0600

    writing functions for components

---
+++ frontend/App.jsx
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NoteCard from "."
+
+function App() {
+  return ( <div >
+    <Signup />
+  </div>
+  )
+}
+
+export default App;
