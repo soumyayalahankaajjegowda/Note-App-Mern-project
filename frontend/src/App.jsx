e60f44e2693890e6d36850dcd4c30b47ee59658d
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-17 21:03:26 -0600

    latest update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
+import Head from "./components/Head"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
+import  Home from "./pages/Home";
+import AddForm from "./pages/AddForm";
+import NoteDetails from "./pages/NoteDetails";
+import EditForm from "./pages/EditForm";
 
 function App() {
   return ( 
@@ -9,6 +13,8 @@ function App() {
       <Routes>
           <Route path={"/"} element={<Home />} />
          <Route path={"/add"} element={<AddForm/>}/>
+         <Route path={"/details/:id"} element={<NoteDetails />} />
+         <Route path={"/edit/:id"} element={<EditForm />} />
       </Routes>
     </Router>
   </>
